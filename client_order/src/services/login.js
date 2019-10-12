1632e9dfa4b0d9f532641c9de75734c55030637d
Mehdi Raza <mehdirazajaffri@gmail.com>
2019-10-12 16:03:31 +0500

    login

--- client_order/src/services/login.js
+++ client_order/src/services/login.js
@@ -1,11 +1,11 @@
-import { Component } from "react";
-import axios from "axios";
+import { Component } from 'react';
+import axios from 'axios';
 
 export default class Service extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.pagesEndPoint = "http:localhost:8000/";
+    this.pagesEndPoint = 'http:localhost:8000/';
   }
 
   api(url, method, data) {
@@ -13,8 +13,8 @@ export default class Service extends Component {
       method: method,
       body: data,
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
       }
     })
       .then(res => res.json())
@@ -29,26 +29,20 @@ export default class Service extends Component {
   }
 
   verifyToken(token) {
-    let res = this.api(
-      "api-token-verify/",
-      "POST",
-      JSON.stringify({ token: token })
-    );
+    let res = this.api('api-token-verify/', 'POST', JSON.stringify({ token: token }));
     return res;
   }
 
   login(user) {
     console.log(user);
-    let res = this.api("api-token-auth/", "POST", JSON.stringify(user));
+    let res = this.api('api-token-auth/', 'POST', JSON.stringify(user));
     return res;
   }
 
   doLogin(user) {
-    return axios
-      .post(`http://localhost:8000/api-token-auth/`, { user })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      });
+    return axios.post(`http://localhost:8000/api-token-auth/`, { user }).then(res => {
+      console.log(res);
+      console.log(res.data);
+    });
   }
 }
